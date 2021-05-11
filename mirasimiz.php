8b45644948245007b9b5c9f816339fc336855f67
sumeyye59 <63192409+sumeyye59@users.noreply.github.com>
2021-05-11 23:21:38 +0300

    Create mirasimiz.php

---
+++ mirasimiz.php
@@ -0,0 +1,44 @@
+<?php include "header.html"; ?>
+
+<div class="jumbotron mirasimizJumbotron">
+	<div class="container">
+		
+		<br><br>
+		<p class="text-dark text-center baslik">GEZİLEBİLECEK YERLER</p>
+		<br><br>
+
+	</div>
+</div>
+
+<div class="jumbotron mirasimizJumbotronbilgi">
+	<div class="container">
+		
+		<div class="text-center marginAuto">
+			<br>
+
+			<h1 class="display-4 mb-4 text-center baslik">Rahime Sultan Camii</h1>
+			<img class="rounded-circle" src="camii.jpeg" height="450" width="600" alt=""/>
+			<h5> 1892 yılında Sapanca Uzunkum’da Abdülmecit’in dördüncü karısı Peruste Rahime Sultan tarafından 
+			Rahime Sultan Cami, yığma taş ve tuğla mimari tekniği kullanılarak inşa edilmiştir.</h5>
+			<h5> İç mimarisi oylama işçiliği ile süslü cami kendine özgü yapısını koruyan ender camiler arasında yerini almaktadır. 
+			Caminin içerisindeki minber, avize ve balkonu da döneme ait orijinal parçalardır.</h5>
+			<h5>17 Ağustos depremi sırasında hasar görmüş ve minaresi yıkılmıştır, tekrardan onarımla restore edilmiştir.</h5>
+
+			<hr class="my-4">
+
+			<h1 class="display-4 mb-4 text-center baslik">Justinianus Köprüsü(Beşköprü)</h1>
+
+			<img class="rounded-circle" src="justinianus.jpg" height="400" width="650" alt=""/>
+			<h5>Bir Erken Bizans Dönemi yapısı olan Beşköprü, Anadolu’nun en göz alıcı anıtsal yapısı olma özelliğini taşımaktadır. </h5>
+			<h5>  Bizans İmparatoru Justinianus tarafından inşa ettirilen yapı, MS 558-560 yıllarında yapılmış olup
+			günümüze kadar sağlam bir şekilde gelmeyi başarmıştır.</h5>
+			<h5> Yaklaşık olarak 365 metre uzunluğa sahip olup köprü, yapıldığı dönem itibariyle kullanılan mimari ve tekniğin altında yatan uzmanlığı ile
+			muhteşem bir örnek teşkil etmektedir.</h5>
+			<h5>Köprünün; doğu ve batı topraklarını tekrardan bir araya getirmek gibi bir amacı temsil etmektedir. </h5>
+		</div>
+		<br>
+		<br>
+	</div>
+</div>
+
+<?php include "footer.php"; ?>
