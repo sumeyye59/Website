7fd1bfbca11d821f1af15099461d72def1fa4049
sumeyye59 <63192409+sumeyye59@users.noreply.github.com>
2021-05-21 19:57:46 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php 
+
+include "header.html"; 
+session_start();
+ob_start();
+
+
+
+if(isset($_SESSION["login"]))
+	header("Location:admin.php");
+
+?>
+
+<div class="jumbotron mb-0">
+<br><br>
+	<div id="loginSayfasi" class="container shadow-lg col-md-4 text-black">
+		<form action="kontrol.php" method="POST">
+			<br>
+			<div class="form-group">
+				<label for="exampleInputEmail1">Email Adresi:</label>
+				<input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="g191210037@sakarya.edu.tr" required="">
+			</div>
+
+			<div class="form-group mb-3">
+				<label for="exampleInputPassword1">Şifre:</label>
+				<input type="password" name="sifre" class="form-control" id="exampleInputPassword1" placeholder="g191210037" required="">
+			</div>
+
+			<button type="submit" class="btn btn-primary">Gönder</button>
+		</form>
+		<br><br>
+	</div>
+	<br>
+	<br><br><br><br><br>
+</div>
+
+<?php include "footer.php"; ?>
